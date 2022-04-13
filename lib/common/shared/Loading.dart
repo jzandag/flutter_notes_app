73d5679250413be5590763e024680ef7ca34fba4
mlph-jandag <john.andag@monstar-lab.com>
2022-04-13 15:35:26 +0800

    Add Loading widget

---
+++ lib/common/shared/Loading.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.teal[700],
+      child: const SpinKitChasingDots(
+        color: Colors.white,
+        size: 50,
+      ),
+    );
+  }
+}
